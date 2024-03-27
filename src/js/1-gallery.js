f2394e3376c9b4175631fd33ca7c3d3087b576c0
Elenagri9580 <150274791+Elenagri9580@users.noreply.github.com>
2024-03-27 18:10:41 +0200

    2-form html-css-js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('Gallery');
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input');
+const message = form.querySelector('textarea');
+
+form.addEventListener('input', onInputTextarea);
+form.addEventListener('submit', handleSubmit);
+
+function onInputTextarea() {
+  //  const textEmail = email.value.trim();
+  //  const textMessage = message.value.trim();
+  // localStorage.setItem("feedback-form-input", textEmail);
+  // localStorage.setItem("feedback-form-textarea", textMessage);
+  const text = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(text));
+}
+
+function saveInputTextArea() {
+  //const saveTextEmail = localStorage.getItem("feedback-form-input");
+  //const saveTextMessage = localStorage.getItem("feedback-form-textarea");
+  // if (saveTextEmail || saveTextMessage) {
+  //     email.value = saveTextEmail;
+  //     message.value = saveTextMessage;
+  // }
+  const saveText = localStorage.getItem('feedback-form-state');
+  if (saveText) {
+    const parsedText = JSON.parse(saveText);
+    email.value = parsedText.email;
+    message.value = parsedText.message;
+  }
+}
+saveInputTextArea();
+
+function handleSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  // localStorage.removeItem("feedback-form-input");
+  // localStorage.removeItem("feedback-form-textarea");
+}
